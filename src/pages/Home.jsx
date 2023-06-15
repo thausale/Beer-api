be9f186aa240196a30444118ff68c8339970018c
Niels Cnops <cnops_niels@hotmail.com>
2023-06-15 12:20:43 +0200

    tested homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { beerData } from "../../public/beerData";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Home = () => {
-  const [beers, setBeers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  //   TODO: once we have API, set default to true, uncomment useEffect and remove beerData
+  const [beers, setBeers] = useState(beerData);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    const fetchBeers = async () => {
-      try {
-        const response = await fetch(`http://localhost/beers`);
-        if (response) {
-          const beersData = await response.json();
-          setBeers(beersData);
-        } else {
-          // Handle error response
-          console.error("Error fetching beers:", response.status);
-        }
-      } catch (error) {
-        // Handle network or other errors
-        console.error("Error fetching beers:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  //   useEffect(() => {
+  //     const fetchBeers = async () => {
+  //       try {
+  //         const response = await fetch(`http://localhost/beers`);
+  //         if (response) {
+  //           const beersData = await response.json();
+  //           setBeers(beersData);
+  //         } else {
+  //           // Handle error response
+  //           console.error("Error fetching beers:", response.status);
+  //         }
+  //       } catch (error) {
+  //         // Handle network or other errors
+  //         console.error("Error fetching beers:", error);
+  //       } finally {
+  //         setLoading(false);
+  //       }
+  //     };
 
-    fetchBeers();
-  }, []);
+  //     fetchBeers();
+  //   }, []);
 
   return (
     <div className="container mt-5">
@@ -36,6 +41,10 @@ const Home = () => {
           {beers.map((beer) => (
             <Col>
               <Card>
+                <Card.Img
+                  variant="top"
+                  src="https://images.unsplash.com/photo-1560807707-8cc77767d783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
+                />
                 <Card.Body>
                   <Card.Title>{beer.name}</Card.Title>
                   <Card.Subtitle className="mb-2 text-muted">
