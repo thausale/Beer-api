3220f3193fe9d42f42c42bee508dc2959146ef22
Birhan Yorukoglu <birhanyorukoglu@gmail.com>
2023-06-15 15:14:20 +0200

    Navbar changed

--- src/components/BeerNavbar.jsx
+++ src/components/BeerNavbar.jsx
@@ -1,7 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Nav, Button } from "react-bootstrap";
-import { useState } from "react";
-import LoginModal from "./LoginModal";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Nav, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import LoginModal from './LoginModal';
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -13,21 +13,28 @@ function App() {
   const handleCloseModal = () => {
     setShowModal(false);
   };
+
   return (
     <div className="App p-4">
-      <Nav activeKey="/home">
+      <Nav activeKey="/home" className="justify-content-between">
         <Nav.Item>
-          <Nav.Link href="/beers">Beers</Nav.Link>
+          <Nav.Link href="/">Untappd</Nav.Link>
         </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/breweries">Breweries</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Button onClick={handleLogin}>Login</Button>
-        </Nav.Item>
+        <div className="d-flex">
+          <Nav.Item>
+            <Nav.Link href="/beers">Beers</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/breweries">Breweries</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Button onClick={handleLogin}>Login</Button>
+          </Nav.Item>
+        </div>
       </Nav>
       <LoginModal show={showModal} handleClose={handleCloseModal} />
     </div>
   );
 }
+
 export default App;
