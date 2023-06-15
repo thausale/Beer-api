197897a75fe60a16a883e5fa9ecf90a5aec4a7a2
Niels Cnops <cnops_niels@hotmail.com>
2023-06-15 13:50:26 +0200

    feat: brewery dummy data

---
+++ public/breweryData.js
@@ -0,0 +1,68 @@
+export const breweries = [
+  {
+    id: 1,
+    name: "Brouwerij Haacht",
+    photo:
+      "https://images.unsplash.com/photo-1560807707-8cc77767d783?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+    Desc: "Primus Haacht Brewery is a renowned Belgian brewery with a rich history and a commitment to producing exceptional beers. Located in the town of Haacht, Belgium, the brewery was founded in 1898 and has since become a symbol of Belgian brewing tradition and craftsmanship.",
+    url: "https://haacht.com/",
+    comments: [
+      {
+        id: 1,
+        name: "joske",
+        comment: "lekkere biertjes",
+      },
+      {
+        id: 1,
+        name: "jefke",
+        comment: "goeieuh biertjes",
+      },
+    ],
+    address: "Provinciesteenweg 28, 3190 Boortmeerbeek",
+    country: "Belgium",
+  },
+  {
+    id: 2,
+    name: "Chimay Brewery",
+    photo:
+      "https://images.unsplash.com/photo-1590758933291-847edee49c21?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    Desc: "Chimay Brewery, also known as Bières de Chimay, is a Belgian Trappist brewery located in the Scourmont Abbey in Chimay, Belgium. Established inside a Cistercian monastery in 1862, the brewery follows traditional brewing methods and is renowned for producing authentic Trappist beers.",
+    url: "https://www.chimay.com/",
+    comments: [
+      {
+        id: 1,
+        name: "Maria",
+        comment: "Love the Trappist beers from Chimay!",
+      },
+      {
+        id: 1,
+        name: "John",
+        comment: "The Chimay Blue is my favorite!",
+      },
+    ],
+    address: "Abbaye de Scourmont 16, 6464 Chimay",
+    country: "Belgium",
+  },
+  {
+    id: 3,
+    name: "Duvel Moortgat Brewery",
+    photo:
+      "https://images.unsplash.com/photo-1562121046-0e73aef5d5a1?ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+    Desc: "Duvel Moortgat Brewery is a Belgian family-owned brewery founded in 1871. Located in Puurs, Belgium, it is famous for producing Duvel, a strong Belgian golden ale. The brewery's commitment to quality and craftsmanship has made Duvel one of the most beloved Belgian beers worldwide.",
+    url: "https://www.duvel.com/",
+    comments: [
+      {
+        id: 1,
+        name: "Sarah",
+        comment: "Duvel is my go-to beer for special occasions!",
+      },
+      {
+        id: 1,
+        name: "Mark",
+        comment: "The Duvel Triple Hop is an absolute delight!",
+      },
+    ],
+    address: "Breendonkdorp 58, 2870 Puurs",
+    country: "Belgium",
+  },
+];
